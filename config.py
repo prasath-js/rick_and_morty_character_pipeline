2fedb66457656a8d3dbc968d26005436008570f7
prasath-js <87058362+prasath-js@users.noreply.github.com>
2026-04-01 16:56:20 +0530

    Pipeline AI: update config.py

--- config.py
+++ config.py
@@ -2,10 +2,15 @@ import os
 
 # Database connection details
 DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_NAME = os.getenv('DB_NAME', 'rick_and_morty')
+DB_NAME = os.getenv('DB_NAME', 'rick_and_morty_db')
 DB_USER = os.getenv('DB_USER', 'user')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
 DB_PORT = os.getenv('DB_PORT', '5432')
 
 # API details
 RICK_AND_MORTY_API_URL = "https://rickandmortyapi.com/api/character"
+
+# Table Names
+BRONZE_TABLE_NAME = 'bronze.bronze_characters'
+SILVER_TABLE_NAME = 'silver.silver_characters'
+GOLD_TABLE_NAME = 'gold.gold_character_names' # As per README, gold_character_names, not gold_characters
